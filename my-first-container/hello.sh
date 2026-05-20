cf68456d6306eee378ea7073bcec4c5a5a76824d
Awosebhami <awosebhami@gmail.com>
2026-05-20 10:44:38 +0100

    Added Docker files - my-first-container with Dockerfile and hello.sh

---
+++ my-first-container/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello from inside a Docker container!"
+echo "This script was copied from my laptop into the container using COPY"
+echo "My name is Awosebhami and I am learning DevOps!"
+
